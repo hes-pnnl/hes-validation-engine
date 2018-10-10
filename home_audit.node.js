3d8fee164d8631548586bf0b22023c55a9adb480
Davis, Prescott H <prescott.davis@pnnl.gov>
2018-10-10 14:47:55 -0700

    Ugh

--- home_audit.node.js
+++ home_audit.node.js
@@ -1228,61 +1228,53 @@ let validationRules = {
     }
 };
 
-let global = global || window;
-
-global.HESValidationEngine = {
-
-    /**
-     * @param {Object} homeValues Key/value pairs. The keys should be identical to the "name" attributes of the
-     * corresponding form fields.
-     * @returns {Object} Keys are the same as in homeValues. Values are error strings. In the event that no
-     * validation rules were violated, an empty object is returned.
-     */
-    validate_home_audit: function(homeValues) {
-        // Pass homeValues into the scope of this file so that validation rules can reference it
-        // without us having to explicitly pass it to every function
-        _homeValues = homeValues;
-        let result = {};
-        result[BLOCKER] = {};
-        result[ERROR] = {};
-        result[MANDATORY] = {};
-        let requiredFields = require('./required_fields.node')(homeValues);
-        let validationRules = require('./home_audit.node');
-        for (var fieldName in requiredFields) {
-            //Because we have two validation rules for one user input, here we check for potential duplicate messages
-            if (undefined === homeValues[fieldName] || '' === homeValues[fieldName] || null === homeValues[fieldName]) {
-                if ((fieldName === 'heating_fuel_1' && (homeValues['heating_type_1'] === 'none' || homeValues['heating_type_1'] === '')) ||
-                    (fieldName === 'heating_fuel_2' && (homeValues['heating_type_2'] === 'none' || homeValues['heating_type_2'] === ''))) {
-                    /*
-                     * If heating_fuel_ is not entered, we must check if heating_type_ is 'none'
-                     * (that is, the user selecting "None").  In this scenario, heating_fuel_ is not required.
-                     * Further, if both are empty, we do not need to see the validation message for both.
-                     */
-                } else if (fieldName === 'hot_water_type' && homeValues['hot_water_fuel'] === '') {
-                    // Do nothing ... avoid duplicate messages
-                } else {
-                    result[MANDATORY][fieldName] = requiredFields[fieldName];
-                }
+/**
+ * @param {Object} homeValues Key/value pairs. The keys should be identical to the "name" attributes of the
+ * corresponding form fields.
+ * @returns {Object} Keys are the same as in homeValues. Values are error strings. In the event that no
+ * validation rules were violated, an empty object is returned.
+ */
+function validate_home_audit (homeValues) {
+    // Pass homeValues into the scope of this file so that validation rules can reference it
+    // without us having to explicitly pass it to every function
+    _homeValues = homeValues;
+    let result = {};
+    result[BLOCKER] = {};
+    result[ERROR] = {};
+    result[MANDATORY] = {};
+    let requiredFields = require('./required_fields.node')(homeValues);
+    for (var fieldName in requiredFields) {
+        //Because we have two validation rules for one user input, here we check for potential duplicate messages
+        if (undefined === homeValues[fieldName] || '' === homeValues[fieldName] || null === homeValues[fieldName]) {
+            if ((fieldName === 'heating_fuel_1' && (homeValues['heating_type_1'] === 'none' || homeValues['heating_type_1'] === '')) ||
+                (fieldName === 'heating_fuel_2' && (homeValues['heating_type_2'] === 'none' || homeValues['heating_type_2'] === ''))) {
+                /*
+                 * If heating_fuel_ is not entered, we must check if heating_type_ is 'none'
+                 * (that is, the user selecting "None").  In this scenario, heating_fuel_ is not required.
+                 * Further, if both are empty, we do not need to see the validation message for both.
+                 */
+            } else if (fieldName === 'hot_water_type' && homeValues['hot_water_fuel'] === '') {
+                // Do nothing ... avoid duplicate messages
+            } else {
+                result[MANDATORY][fieldName] = requiredFields[fieldName];
             }
         }
-        for (let [fieldName, value] of Object.entries(homeValues)) {
-            if (value === null || value === undefined || value.length === 0) {
-                continue;
-            }
-            console.log(typeof(validationRules[fieldName]));
-            if (typeof(validationRules[fieldName]) !== 'function') {
-                console.error("Missing a validation rule for field " + fieldName);
-                continue;
-            }
-            let validationResult = validationRules[fieldName](value);
-            if (undefined !== validationResult) {
-                if (undefined !== validationResult['message']) {
-                    result[validationResult['type']][fieldName] = validationResult['message'];
-                }
-            }
-        }
-        return result;
     }
+    for (let [fieldName, value] of Object.entries(homeValues)) {
+        if (value === null || value === undefined || value.length === 0) {
+            continue;
+        }
+        if (typeof(validationRules[fieldName]) !== 'function') {
+            continue;
+        }
+        let validationResult = validationRules[fieldName](value);
+        if (undefined !== validationResult) {
+            if (undefined !== validationResult['message']) {
+                result[validationResult['type']][fieldName] = validationResult['message'];
+            }
+        }
+    }
+    return result;
 }
 
-module.exports = HESValidationEngine.validate_home_audit;
+module.exports = validate_home_audit;
