79d27d25655e9dc6f4bc25d08f56910d37335881
Davis, Prescott H <prescott.davis@pnnl.gov>
2019-06-18 10:39:59 -0700

    Keep always inclusive until LBNL updates moved

--- type_rules.node.js
+++ type_rules.node.js
@@ -21,6 +21,7 @@ let TypeRules = {
      * @param {bool} inclusive
      */
     _float: function(value, min, max, inclusive = true) {
+        inclusive = true; // TODO: Remove when LBNL Model Updates are pushed
         if (value != parseFloat(value)) {
             return value + " is not a number."
         }
