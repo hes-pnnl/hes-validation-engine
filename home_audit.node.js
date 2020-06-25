6560e8fff22320814fa6ccb56283a0c0cf6ac800
Davis, Prescott H <prescott.davis@pnnl.gov>
2020-06-25 16:08:43 -0700

    wall_area check for window_area should be exclusive

--- home_audit.node.js
+++ home_audit.node.js
@@ -778,7 +778,8 @@ let validationRules = {
             return invalidWall;
         }
         if (wallArea) {
-            return this._get_wall_validation(value, side, new Validation(TypeRules._float(value, 0, wallArea), BLOCKER));
+            // NOTE: While the XSD def is inclusive, the wall area check is exclusive
+            return this._get_wall_validation(value, side, new Validation(TypeRules._float(value, 0, wallArea, false), BLOCKER));
         }
     },
 
