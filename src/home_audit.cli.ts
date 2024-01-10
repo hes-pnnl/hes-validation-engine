bfdd87c2caa4cfab1feae840499ad284e6c55e38
Benjamin Stürmer <bstuermer@eno.de>
2024-01-10 11:12:32 +0100

    Got cli file compiling

--- src/home_audit.cli.ts
+++ src/home_audit.cli.ts
@@ -6,21 +6,17 @@
  * node home_audit.cli.js '{"solar_electric_capacity_known" : "3"}'
  * > {"solar_electric_capacity_known":"3 is outside the allowed range (0 - 1)"}
  */
-import fs from "../fs";
-import getNestedValidationMessages from './home_audit.ts';
+import fs from 'fs';
+import { getNestedValidationMessages } from './home_audit';
 
-let input: string = process.argv[2];
+let input_string: string = process.argv[2];
 
 // Check if the input is a file path and read the file
-if(fs.existsSync(input)) {
-    input = fs.readFileSync(input, 'utf8');
+if(fs.existsSync(input_string)) {
+    input_string = fs.readFileSync(input_string, 'utf8');
 }
 
-const obj: object = JSON.parse(input.toString());
-
-// Theoretically, a caller should be passing only the actual building definition, but to be user
-// friendly, if they pass an entire HES JSON object, we will extract out the building_unit
-// object, which is what we are actually able to validate
-const result = getNestedValidationMessages(obj.building_unit || obj);
+let input_obj: any = JSON.parse(input_string.toString());
+const result = getNestedValidationMessages(input_obj.building_unit || input_obj);
 
 console.log(JSON.stringify(result, null, 2));
