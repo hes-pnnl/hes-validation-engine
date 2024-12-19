133f28d45373e4980a50d9e0559417253f193c42
Yan Zhao <zhaoyan1555@gmail.com>
2024-12-18 16:23:14 -0800

    Update blockers.ts

--- src/blockers.ts
+++ src/blockers.ts
@@ -1,5 +1,5 @@
 // Maps building jsonpath to values mappings that will fail database constraints
-const strLength = (val:string, [min, max]:[number, number]) => val.length > max || val.length < min 
+const strLength = (val:string, [min, max]:[number, number]) => val?.length > max || val?.length < min
     ? `The text length for this value should be between ${min} and ${max}.  Current length is ${val.length}`
     : undefined;
 
